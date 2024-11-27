9521ad12fd0c6543a4084408b191beae5c9629c9
fathima nahla pk <nahalanahlu@gmail.com>
2024-11-27 14:09:21 +0530

    completed section

--- src/pages/Navbar/Navbar.jsx
+++ src/pages/Navbar/Navbar.jsx
@@ -1,43 +1,57 @@
-import React from 'react'
-import Logo from '../../assets/images/logo.png'
-import cartcount from '../../assets/images/cart-count.png'
-import search from '../../assets/images/search.png'
-import heart from '../../assets/images/heart.png'
-import cart from '../../assets/images/cart.png'
-import { Link } from 'react-router-dom'
-import './Navbar.css'
-import { useNavigate } from 'react-router-dom'
-
+import React, { useState } from 'react';
+import Logo from '../../assets/images/logo.png';
+import cartcount from '../../assets/images/cart-count.png';
+import search from '../../assets/images/search.png';
+import heart from '../../assets/images/heart.png';
+import cart from '../../assets/images/cart.png';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false); 
   const navigate = useNavigate();
 
-    const handleNavigation = ()=>{
-        navigate('/')
-    }
+  const handleNavigation = (path) => {
+    navigate(path);
+    setMenuOpen(false); 
+  };
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen); 
+  };
+
   return (
     <div>
       <div className='navbar'>
+        
         <div className='logo'>
-            <img src={Logo} alt="Logo" />
-            
+          <img src={Logo} alt="Logo" />
         </div>
-        <ul className='menu'>
-            <li onClick={()=>handleNavigation('/')}>Home</li>
-            <li><Link to='/shop'>Shop</Link></li>
-            <li><Link to='/about'>About</Link></li>
-            <li><Link to='/contact'>Contact</Link></li>
+
+        
+        <div className='icons-mobile' onClick={toggleMenu}>
+          <i className={`fas ${menuOpen ? 'fa-times' : 'fa-bars'}`} />
+        </div>
+
+        
+        <ul className={`menu ${menuOpen ? 'menu-open' : ''}`}>
+          <li onClick={() => handleNavigation('/')}>Home</li>
+          <li><Link to='/shop' onClick={() => setMenuOpen(false)}>Shop</Link></li>
+          <li><Link to='/about' onClick={() => setMenuOpen(false)}>About</Link></li>
+          <li><Link to='/contact' onClick={() => setMenuOpen(false)}>Contact</Link></li>
         </ul>
+
+        
         <div className='icons'>
-            <Link className='cart-count'><img src={cartcount} alt="Logo" /></Link>
-            <Link className='search'><img src={search} alt="Logo" /></Link>
-            <Link className='love'><img src={heart} alt="Logo" /></Link>
-            <Link className='cart'><img src={cart} alt="Logo" /></Link>
+          <Link className='cart-count'><img src={cartcount} alt="Cart Count" /></Link>
+          <Link className='search'><img src={search} alt="Search" /></Link>
+          <Link className='love'><img src={heart} alt="Wishlist" /></Link>
+          <Link className='cart'><img src={cart} alt="Cart" /></Link>
         </div>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Navbar
-
+export default Navbar;
